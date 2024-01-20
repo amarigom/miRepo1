c010b68c7e3b32bf5edcb4552e14a020d0996b46
amarigom <andreamarigomez@gmail.com>
2024-01-20 09:55:03 -0300

    pase a mayusculas las propiedades de la clase Tarea.js

--- src/pure/task.jsx
+++ src/pure/task.jsx
@@ -6,10 +6,10 @@ import {Task} from '../models/Task';
 const TaskComponent = ({task}) => {
     return (
         <div>
-            <h2> nombre: {task.Name}</h2>
-            <h2> descripcion: {task.Description}</h2>
-            <h2> completed: {task.Completed}</h2>
-            <h2> level: {task.Level}</h2>
+            <h2> Nombre: {task.Name}</h2>
+            <h2> Descripcion: {task.Description}</h2>
+            <h2> Completed: {task.Completed}</h2>
+            <h2> Level: {task.Level}</h2>
             
         </div>
     );
